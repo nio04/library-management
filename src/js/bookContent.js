03057b934c6f70d68b4b1c24116e7a69b7f8f9aa
nio04 <nishatislam3108@gmail.com>
2024-02-07 02:25:08 +0600

    added js book lists arrayOfObject

---
+++ src/js/bookContent.js
@@ -0,0 +1,110 @@
+import * as images from '../asset/img/*.jpg';
+
+export const bookLists = [
+  {
+    id: crypto.randomUUID(),
+    title: "book name A",
+    authorName: "author name A",
+    genre: ["comedy", "romance", "slice of life", "actions"],
+    imgUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2Fvectors%2Fbook-cover-design.html&psig=AOvVaw2m52QUXSf6nMPouKEF_w80&ust=1707337001628000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCNCL4IzEl4QDFQAAAAAdAAAAABAE"
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "book name B",
+    authorName: "author name B",
+    genre: ["comedy", "slice of life"],
+    imgUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2Fvectors%2Fbook-cover-design.html&psig=AOvVaw2m52QUXSf6nMPouKEF_w80&ust=1707337001628000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCNCL4IzEl4QDFQAAAAAdAAAAABAE"
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "book name C",
+    authorName: "author name C",
+    genre: ["comedy", "romance", "slice of life", "actions", "tragedy"],
+    imgUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2Fvectors%2Fbook-cover-design.html&psig=AOvVaw2m52QUXSf6nMPouKEF_w80&ust=1707337001628000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCNCL4IzEl4QDFQAAAAAdAAAAABAE"
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "book name D",
+    authorName: "author name D",
+    genre: ["comedy", "romance", "actions"],
+    imgUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2Fvectors%2Fbook-cover-design.html&psig=AOvVaw2m52QUXSf6nMPouKEF_w80&ust=1707337001628000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCNCL4IzEl4QDFQAAAAAdAAAAABAE"
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "book name E",
+    authorName: "author name E",
+    genre: ["comedy", "romance", "slice of life", "actions", "adults"],
+    imgUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2Fvectors%2Fbook-cover-design.html&psig=AOvVaw2m52QUXSf6nMPouKEF_w80&ust=1707337001628000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCNCL4IzEl4QDFQAAAAAdAAAAABAE"
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "book name F",
+    authorName: "author name F",
+    genre: ["comedy", "slice of life", "actions", "NTR"],
+    imgUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2Fvectors%2Fbook-cover-design.html&psig=AOvVaw2m52QUXSf6nMPouKEF_w80&ust=1707337001628000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCNCL4IzEl4QDFQAAAAAdAAAAABAE"
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "book name G",
+    authorName: "author name G",
+    genre: ["heroic", "historical", "slice of life", "actions"],
+    imgUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2Fvectors%2Fbook-cover-design.html&psig=AOvVaw2m52QUXSf6nMPouKEF_w80&ust=1707337001628000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCNCL4IzEl4QDFQAAAAAdAAAAABAE"
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "book name H",
+    authorName: "author name H",
+    genre: ["comedy", "romance", "slice of life", "actions", "drama"],
+    imgUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2Fvectors%2Fbook-cover-design.html&psig=AOvVaw2m52QUXSf6nMPouKEF_w80&ust=1707337001628000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCNCL4IzEl4QDFQAAAAAdAAAAABAE"
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "book name I",
+    authorName: "author name I",
+    genre: ["comedy", "NTR", "actions"],
+    imgUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2Fvectors%2Fbook-cover-design.html&psig=AOvVaw2m52QUXSf6nMPouKEF_w80&ust=1707337001628000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCNCL4IzEl4QDFQAAAAAdAAAAABAE"
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "book name J",
+    authorName: "author name J",
+    genre: ["Bloody", "adults", "slice of life", "actions"],
+    imgUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2Fvectors%2Fbook-cover-design.html&psig=AOvVaw2m52QUXSf6nMPouKEF_w80&ust=1707337001628000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCNCL4IzEl4QDFQAAAAAdAAAAABAE"
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "book name K",
+    authorName: "author name K",
+    genre: ["comedy", "romance", "historical", "drama"],
+    imgUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2Fvectors%2Fbook-cover-design.html&psig=AOvVaw2m52QUXSf6nMPouKEF_w80&ust=1707337001628000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCNCL4IzEl4QDFQAAAAAdAAAAABAE"
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "book name L",
+    authorName: "author name L",
+    genre: ["comedy", "slice of life", "drama", "actons", "adults"],
+    imgUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2Fvectors%2Fbook-cover-design.html&psig=AOvVaw2m52QUXSf6nMPouKEF_w80&ust=1707337001628000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCNCL4IzEl4QDFQAAAAAdAAAAABAE"
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "book name M",
+    authorName: "author name M",
+    genre: ["comedy", "school", "politics", "NTR" ],
+    imgUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2Fvectors%2Fbook-cover-design.html&psig=AOvVaw2m52QUXSf6nMPouKEF_w80&ust=1707337001628000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCNCL4IzEl4QDFQAAAAAdAAAAABAE"
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "book name N",
+    authorName: "author name N",
+    genre: ["comedy", "actions", "school", "animation", "webtoons"],
+    imgUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2Fvectors%2Fbook-cover-design.html&psig=AOvVaw2m52QUXSf6nMPouKEF_w80&ust=1707337001628000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCNCL4IzEl4QDFQAAAAAdAAAAABAE"
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "book name O",
+    authorName: "author name O",
+    genre: ["comedy", "romance", "slice of life", "actions"],
+    imgUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2Fvectors%2Fbook-cover-design.html&psig=AOvVaw2m52QUXSf6nMPouKEF_w80&ust=1707337001628000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCNCL4IzEl4QDFQAAAAAdAAAAABAE"
+  },
+
+]
