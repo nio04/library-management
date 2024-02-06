a07a9655cb7b43d6152b4e188c89cc267af62073
nio04 <nishatislam3108@gmail.com>
2024-02-07 02:08:59 +0600

    js- back button navigation complete

--- src/js/view/libraryView.js
+++ src/js/view/libraryView.js
@@ -1,20 +1,21 @@
 import * as helper from "../helper";
-
-const parent = document.querySelector(".library-management");
+const welcomeSection=document.querySelector(".welcome")
+const libraryManagement = document.querySelector(".library-management");
 const viewAllBooksSection = document.querySelector(".view-books");
 const searchBookOfflineSection = document.querySelector(".search-books");
 const uploadBookSection = document.querySelector(".upload-book");
 const searchBookOnlineSection = document.querySelector(".search-online");
 const issueBookSection = document.querySelector(".issue-book");
+const backBtn = document.querySelector("#back-btn");
 
 function hideAllSections() {
-  helper.hideEl(parent, viewAllBooksSection, searchBookOfflineSection, uploadBookSection, searchBookOnlineSection, issueBookSection);
+  helper.hideEl(libraryManagement, viewAllBooksSection, searchBookOfflineSection, uploadBookSection, searchBookOnlineSection, issueBookSection);
 }
 
 function showTargetElement(ev) {
   // 1) check which btn is checked
   const checkedBtn = ev.target.dataset.pointer;
- if(!checkedBtn) return
+  if(!checkedBtn) return
 
   // 2) select that parent el by dataset && remove HIDDEN class
   const parentEl = document.querySelector(`.${ checkedBtn }`);
@@ -23,6 +24,27 @@ function showTargetElement(ev) {
 }
 
 function libraryPageControl(ev) {
+  if (backBtn) {
+    const target = ev.target.dataset.backTo;
+
+    if (target === "mainPage") {
+      // 1) hide all the sections
+      helper.hideEl(libraryManagement);
+      // 2) show welcome page
+      helper.showEl(welcomeSection);
+
+    } else if(target==="libraryPage") {
+      // BUTTON = "BACK TO LIBRARY PAGE"
+
+      // 1) HIDE CURRENT PAGE (PARENT);
+      const currentPage = ev.target.parentElement.className;
+      helper.hideEl(document.querySelector(`.${ currentPage }`));
+
+      // 2) SHOW DESIRE PAGE
+      helper.showEl(libraryManagement, viewAllBooksSection, searchBookOfflineSection, uploadBookSection, searchBookOnlineSection, issueBookSection);
+    }
+  }
+
   if (!ev.target.closest(".library-management") || ev.target.classList.contains("library-management")) return;
 
   // HIDE ALL THE SECTION
--- src/js/view/welcomeView.js
+++ src/js/view/welcomeView.js
@@ -1,12 +1,21 @@
-const section = document.querySelector(".welcome");
+import * as helper from "../helper";
+
+const welcomeSection = document.querySelector(".welcome");
 const btn = document.querySelector(".welcome-btn");
 const libraryPage=document.querySelector(".library-management")
+const viewAllBooksSection = document.querySelector(".view-books");
+const searchBookOfflineSection = document.querySelector(".search-books");
+const uploadBookSection = document.querySelector(".upload-book");
+const searchBookOnlineSection = document.querySelector(".search-online");
+const issueBookSection = document.querySelector(".issue-book");
 
 function welcomeViewControl() {
-  section.classList.add("hidden");
+  // welcomeSection.classList.add("hidden");
+  helper.hideEl(welcomeSection)
 
   // SHOW LIBRARY-MANAGEMENT PAGE
-  libraryPage.classList.remove("hidden")
+  // libraryPage.classList.remove("hidden");
+  helper.showEl(libraryPage, viewAllBooksSection, searchBookOfflineSection, uploadBookSection, searchBookOnlineSection, issueBookSection);
 }
 
 // EVENT
