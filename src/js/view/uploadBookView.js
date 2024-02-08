b1e4f9f88901d0341b88950413c09f255924bbaf
nio04 <nishatislam3108@gmail.com>
2024-02-08 10:33:49 +0600

    chnaged img file extension // js- uploadBook- capture user input ONLY // html- added html form valid for uploading book // next- add js form validity

--- src/js/view/uploadBookView.js
+++ src/js/view/uploadBookView.js
@@ -1,10 +1,49 @@
-const uploadBtn = document.querySelector("#form-upload");
+let bookName
+let img
+let authorName
+let bookGenre
+let bookQuantity
+let uploadBtn = document.querySelector("#form-upload");
 
 function uploadBookControl(ev) {
-   ev.preventDefault();
+  ev.preventDefault();
 
-  console.log("uplaoded");
+  // ON SUBMIT, GET VALUE OF INPUT'S
+  getInputValue();
+
+  // compute - Book IMAGE - src
+  const bookImg = img.files[0];
+
+  // VALIDATE ALL THE NAMING
+  validateNaming();
+
+  const currentBook = {
+    id: crypto.randomUUID(),
+    bookName,
+    bookImg,
+    authorName,
+    bookGenre,
+    bookQuantity,
+    };
+
+
+  // console.log(currentBook);
+
 }
 
-uploadBtn.addEventListener("click", uploadBookControl)
+const getInputValue=() => {
+  bookName = document.querySelector("#book-name__input").value;
+  img = document.querySelector("#book-image__input");
+  authorName = document.querySelector("#book-author__input").value;
+  bookGenre = document.querySelector("#book-genre__input").value;
+  bookQuantity = document.querySelector("#book-quantity__input").value;
+}
+
+// VALIDATE NAMING
+const validateNaming = book => {
+  const regex = /null/gi;
+  return book.match(regex)
+}
+
+document.addEventListener("submit", uploadBookControl)
 export default uploadBookControl;
